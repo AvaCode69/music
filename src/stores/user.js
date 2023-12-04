00dc716c088b0ce23eda7655fbfc04ecf423b8e0
AvaCode69 <reacthmd34@gmail.com>
2023-12-04 11:45:59 +0100

    Persisting User Authentication & Setting up Login

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,6 +1,12 @@
+// user.js
+
 import { defineStore } from 'pinia'
 import { auth, usersCollection } from '../includes/firebase'
-import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import {
+  createUserWithEmailAndPassword,
+  updateProfile,
+  signInWithEmailAndPassword
+} from 'firebase/auth'
 import { doc, setDoc } from 'firebase/firestore'
 
 export default defineStore('user', {
@@ -9,26 +15,26 @@ export default defineStore('user', {
   }),
   actions: {
     async register(values) {
-      try {
-        const userCred = await createUserWithEmailAndPassword(auth, values.email, values.password)
+      const userCred = await createUserWithEmailAndPassword(auth, values.email, values.password)
 
-        const userDocRef = doc(usersCollection, userCred.user.uid)
-        await setDoc(userDocRef, {
-          name: values.name,
-          email: values.email,
-          age: values.age,
-          country: values.country
-        })
+      const userDocRef = doc(usersCollection, userCred.user.uid)
+      await setDoc(userDocRef, {
+        name: values.name,
+        email: values.email,
+        age: values.age,
+        country: values.country
+      })
 
-        await updateProfile(userCred.user, {
-          displayName: values.name
-        })
+      await updateProfile(userCred.user, {
+        displayName: values.name
+      })
 
-        this.userLoggedIn = true
-      } catch (error) {
-        console.error('Error during user registration:', error.message)
-        throw error
-      }
+      this.userLoggedIn = true
+    },
+
+    async authenticate(values) {
+      await signInWithEmailAndPassword(auth, values.email, values.password)
+      this.userLoggedIn = true
     }
   }
 })
